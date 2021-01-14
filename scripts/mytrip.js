4ddf431c59abe9b8b345824e2ce943f2dd7d511d
Youngmin Park <43109589+ympark99@users.noreply.github.com>
2021-01-14 20:53:41 +0900

    Add files via upload

--- scripts/mytrip.js
+++ scripts/mytrip.js
@@ -1,53 +1,82 @@
-var MARKER_LABELS = "ABCDEFGHIJKLMNOPQRSTUVWXYZ";
-var map;
+var MARKER_LABELS = 'ABCDEFGHIJKLMNOPQRSTUVWXYZ';
 
-$(function(){
+var map;
+var markers = {};
+
+$(function() {
     var myTrips = Cookies.getJSON('MYTRIPS');
 
-    if(!myTrips)
-        myTrips = [];
+    if (!myTrips)
+    myTrips = [];
 
     showMap();
     generateMyTripList(myTrips);
 });
 
-function showMap(){
-    map = new google.maps.Map(document.getElementById('map'),{
-        zoom : 12,
-        center : {
-            lat : 33.3617,
-            lng : 126.5292
-        }
-    });
-}
-
-function generateMyTripList(list){
-    var bounds = new google.maps.LatLngBounds(); // 마커 여러개일때 영역을 한 번에 보여줌
+function generateMyTripList(list) {
+    var bounds = new google.maps.LatLngBounds();
     var $list = $('#mytrip-list');
 
-    for(var i=0; i<list.length; i++){
-        var myTrip = list[i];
+    for (var i = 0; i < list.length; i++) {
+    var myTrip = list[i];
 
-        var pos = {
-            lat : myTrip.x,
-            lng : myTrip.y
-        };
+    var pos = {
+    lat: myTrip.x,
+    lng: myTrip.y
+    };
 
-        var markerlabel = MARKER_LABELS[i];
+    var markerLabel = MARKER_LABELS[i];
 
-        var $item = $('#mytrip-item-template').clone().removeAttr('id');
-        $item.find('.item-name').html(markerlabel + '. ' + myTrip.name);
-        $item.find('.item-city-name').html(myTrip.cityName);
+    var $item = $('#mytrip-item-template').clone().removeAttr('id');
 
-        $list.append($item);
+    $item.data('id', myTrip.id);
+    $item.find('.item-name').html(markerLabel + '. ' + myTrip.name);
+    $item.find('.item-city-name').html(myTrip.cityName);
 
-        new google.maps.Marker({
-            position : pos,
-            label: markerlabel,
-            map : map
-        });
+    $item.find('.item-remove').click(function() {
+        var $elem = $(this).closest('.mytrip-item');
+        var id = $elem.data('id');
+        $elem.remove();
+        markers[id].setMap(null);
+        markers[id] = null;
+        var newList = removeFromList(list, id);
+        Cookies.set('MYTRIPS', newList);
+    });
 
-        bounds.extend(pos);
+    $list.append($item);
+
+    var marker = new google.maps.Marker({
+        position: pos,
+        label: markerLabel,
+        map: map
+    });
+
+    markers[myTrip.id] = marker;
+
+    bounds.extend(pos);
     }
+
     map.fitBounds(bounds);
+
+}
+
+function removeFromList(list, id) {
+    var index = -1;
+
+    for (var i = 0; i < list.length; i++) {
+        if (list[i].id === id) {
+            index = i;
+            break;
+        }
+    }
+
+    if (index !== -1) {
+    list.splice(index, 1);
+    }
+
+    return list;
+}
+
+function showMap() {
+    map = new google.maps.Map(document.getElementById('map'));
 }
